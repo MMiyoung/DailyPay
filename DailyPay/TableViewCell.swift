f3a93464a769e5d3f7301882d3c8e30c2007cc30
MYMiyoung <al2969@naver.com>
2022-12-29 16:38:41 +0900

    Add AutoLayout_MainView, AddView

--- DailyPay/TableViewCell.swift
+++ DailyPay/TableViewCell.swift
@@ -3,21 +3,20 @@
 //  DailyPay
 //
 //  Created by 김미영 on 2022/10/13.
-// 이거슨 테스트여 테스트
 //
-//import UIKit
-//
-//class TableViewCell: UITableViewCell {
-//
-//    override func awakeFromNib() {
-//        super.awakeFromNib()
-//        // Initialization code
-//    }
-//
-//    override func setSelected(_ selected: Bool, animated: Bool) {
-//        super.setSelected(selected, animated: animated)
-//
-//        // Configure the view for the selected state
-//    }
-//
-//}
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
