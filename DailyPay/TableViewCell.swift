740ae9017962265109e31e603b2da3534528c0cc
MYMiyoung <al2969@naver.com>
2022-12-29 11:04:19 +0900

    commit test

--- DailyPay/TableViewCell.swift
+++ DailyPay/TableViewCell.swift
@@ -4,20 +4,20 @@
 //
 //  Created by 김미영 on 2022/10/13.
 //
-
-import UIKit
-
-class TableViewCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
-}
+//
+//import UIKit
+//
+//class TableViewCell: UITableViewCell {
+//
+//    override func awakeFromNib() {
+//        super.awakeFromNib()
+//        // Initialization code
+//    }
+//
+//    override func setSelected(_ selected: Bool, animated: Bool) {
+//        super.setSelected(selected, animated: animated)
+//
+//        // Configure the view for the selected state
+//    }
+//
+//}
